01eb1044044c13fa5aef96054d44a8dccf7380e6
Andrés Lucas Enciso <andresin87@gmail.com>
2016-06-27 00:00:00 +0200

    ADD masonry component to the body of the page.

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import ReactDom from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import {HeaderApp} from './components';
+import {HeaderApp, Gallery} from './components';
 
 class App extends React.Component {
   render () {
@@ -14,6 +14,16 @@ class App extends React.Component {
       <div>
         <MuiThemeProvider>
           <HeaderApp/>
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <Gallery
+            elements={[
+            {key:"1",src:"https://randomuser.me/api/portraits/women/48.jpg"},
+            {key:"2",src:"https://randomuser.me/api/portraits/men/53.jpg"},
+            {key:"3",src:"https://randomuser.me/api/portraits/women/31.jpg"},
+            {key:"4",src:"https://randomuser.me/api/portraits/men/27.jpg"}
+          ]}
+          />
         </MuiThemeProvider>
       </div>
     );
